3f4011206e2c0879e03f43123a522d537722880e
김태훈 <xogns5@naver.com>
2024-03-29 11:09:46 +0900

    📝docs: #S10P21C101-361 requirement 수정2
    Former-commit-id: 43395c43f02e12c29a088b7b12943409032eec2b

--- ai/app/main.py
+++ ai/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, File, UploadFile, Form
 import tensorflow as tf
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from keras.models import load_model
+#from keras.models import load_model
 from fastapi.responses import JSONResponse
 import requests
 from io import BytesIO
@@ -13,6 +13,8 @@ from typing import Optional
 from app.classes.doodle import ref as doodle_ref
 import cv2
 import boto3
+import dlib
+import io
 
 app = FastAPI()
 
@@ -47,25 +49,65 @@ class list(BaseModel):
 
 #################################################################
 
-# 모델 경로 지정
+# 낙서 모델 경로 지정
 model_path = './app/model/thmodel2.h5'
 # doodle_model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(reduction=tf.keras.losses.Reduction.NONE), metrics=['accuracy'])
 try:
     # 모델 로드
     doodle_model = tf.keras.models.load_model(model_path)
-    print("#################################Model loaded successfully.")
+    print("####################Doodle Model loaded successfully.")
 
     # 모델 컴파일
     # doodle_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     # doodle_model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(reduction=tf.keras.losses.Reduction.NONE), metrics=['accuracy'])
-    print("Model compiled successfully.")
+    # print("Model compiled successfully.")
 except FileNotFoundError as e:
-    print("@@@@@@@@@@@@@@@@@@@@@@@@@@File not found:", str(e))
+    print("@@@@@@@@@@@@@@@@@@@@File not found:", str(e))
 except Exception as e:
-    print("!!!!!!!!!!!!!!!!!!!!!!!Error loading model:", str(e))
+    print("!!!!!!!!!!!!!!!!!!!!Error loading DOODLE model:", str(e))
 
 ####################################################################
     
+# 감정 모델 경로 지정
+emotion_model_path = './app/model/emotion_model.hdf5'
+# 얼굴 인식
+try:
+    # 표정 가중치 모델
+    emotion_model = tf.keras.models.load_model(emotion_model_path)
+
+    # 모델 전처리 위한 dlib
+    predictor = dlib.shape_predictor('./app/model/shape_predictor_68_face_landmarks.dat')
+    face_cascade = cv2.CascadeClassifier('./app/model/haarcascade_frontalface_default.xml')
+    print("####################Emotion Model loaded successfully.")
+
+except FileNotFoundError as e:
+    print("@@@@@@@@@@@@@@@@@@@@File not found", str(e))
+except Exception as e:
+    print("!!!!!!!!!!!!!!!!!!!!Error loading EMOTION model", str(e))
+
+###################################################################
+    
+# O,X 판별 모델 경로 지정
+action_model_path = './app/model/model_unquant.tflite'
+# 동작 인식
+try:
+    # 표정 가중치 모델
+    action_model = tf.lite.Interpreter(action_model_path)
+    action_model.allocate_tensors()
+
+    # 모델 입력 및 출력 텐서 인덱스 가져오기
+    input_details = action_model.get_input_details()
+    output_details = action_model.get_output_details()
+    print("####################Action Model loaded successfully.")
+
+except FileNotFoundError as e:
+    print("@@@@@@@@@@@@@@@@@@@@File not found", str(e))
+except Exception as e:
+    print("!!!!!!!!!!!!!!!!!!!!Error loading ACTION model", str(e))
+
+###################################################################
+    
+
 s3 = boto3.client(
     "s3", aws_access_key_id="AKIARI2J3RH4QEK7CPSB", aws_secret_access_key="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"
 )
@@ -119,9 +161,89 @@ async def analyze_object(file: UploadFile = File(...), filename: Optional[str] =
     try:
         # 업로드된 파일을 읽음
         contents = await file.read()
+
+        # 업로드된 파일 전처리
+        image_stream = io.BytesIO(contents)
+        image_stream.seek(0)
+        frame = cv2.imdecode(np.frombuffer(image_stream.read(), np.uint8), cv2.IMREAD_COLOR)
+    
+        # # 표정 라벨링
+        expression_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+        # # 얼굴인식을 위해 gray 변환
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # 얼굴 인식
+        # scaleFactor이 1에 가까울수록 표정 인식이 잘 되고 멀 수록 잘 안됨
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
         
-        
+        #region 얼굴이 인식되면 표정을 인식
+        for (x, y, w, h) in faces:
+            # 얼굴 크기에 알맞도록 사각형 그리기
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+            # 얼굴 크기 반환
+            face_roi = gray[y:y+h, x:x+w]
+
+            # 표정을 인식하기 위해 표정 dataset과 똑같은 사이즈 변환
+            # dataset 이미지와 입력된 얼굴의 크기가 다르면 error 발생
+            face_roi = cv2.resize(face_roi, (64, 64))
+            face_roi = np.expand_dims(face_roi, axis=-1)
+            face_roi = np.expand_dims(face_roi, axis=0)
+            face_roi = face_roi / 255.0
+
+            # 모델을 통해 표정 분석
+            output = emotion_model.predict(face_roi)[0]
+
+            # 해당 표정의 값 반환
+            expression_index = np.argmax(output)
+
+            # 표정에 따른 label 값 저장
+            expression_label = expression_labels[expression_index]
+
+            if expression_label == filename:
+                return True
+            else:
+                return False
+
     
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
         
+@app.post("/ai/analyze/action")
+async def analyze_object(file: UploadFile = File(...), filename: Optional[str] = Form(None)):
+    try:
+        # 업로드된 파일을 읽음
+        contents = await file.read()
+
+        contents_stream = BytesIO(contents)
+        # 입력 이미지 경로 설정
+        input_image = Image.open(contents_stream)
+        # 모델 입력 크기에 맞게 이미지 크기 조정
+        input_image = input_image.resize((224, 224))
+        # 이미지를 numpy 배열로 변환하고 정규화
+        input_image = np.array(input_image, dtype=np.float32) / 255.0
+        # 모델 입력 형태에 맞게 차원 추가
+        input_image = np.expand_dims(input_image, axis=0)
+
+        
+        # 추론 실행
+        action_model.set_tensor(input_details[0]['index'], input_image)
+        action_model.invoke()
+
+        # 추론 결과 가져오기
+        output = action_model.get_tensor(output_details[0]['index'])
+        predicted_class = np.argmax(output)
+
+        # 라벨 해석
+        labels = ["O", "X", "N/A"]
+        predicted_label = labels[predicted_class]
+
+        if predicted_label == filename:
+            return True
+        else:
+            return False
+
+    
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
