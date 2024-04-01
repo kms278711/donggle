f6f6bc72a0b60c0c090e4fddb49e238b2d9ea417
김민주 <ducco705@snu.ac.kr>
2024-04-01 15:13:25 +0900

    :construction: feat: #S10P22C101-386 이미지 프리로딩 진행중...

--- frontend/lib/presentation/pages/home/component/book/book_detail.dart
+++ frontend/lib/presentation/pages/home/component/book/book_detail.dart
@@ -2,6 +2,8 @@ import 'dart:math';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_styled_toast/flutter_styled_toast.dart';
+import 'package:frontend/core/theme/constant/app_colors.dart';
 import 'package:frontend/core/theme/constant/app_icons.dart';
 import 'package:frontend/core/theme/custom/custom_font_style.dart';
 import 'package:frontend/core/utils/component/buttons/green_button.dart';
@@ -57,7 +59,6 @@ class _BookDetailState extends State<BookDetail> {
 
     await bookModel.getBookDetail(accessToken, widget.bookId);
 
-
     if (mounted) {
       bookPageImagePath = bookModel.BookDetail['bookPageImagePath'];
       for (String imagePath in bookPageImagePath) {
@@ -66,7 +67,7 @@ class _BookDetailState extends State<BookDetail> {
           context,
         );
       }
-      if(mounted) {
+      if (mounted) {
         // 데이터 로딩 완료 후 상태 업데이트
         setState(() {
           bookTitle = bookModel.BookDetail['title'];
@@ -217,7 +218,6 @@ class _BookDetailState extends State<BookDetail> {
                         GreenButton("처음부터", onPressed: () {
                           Navigator.of(context).pop();
                           bookModel.progresses[index].isDone = false;
-                          print("----------- isDone: ${bookModel.progresses[index].isDone}");
                           globalRouter.pushReplacement('/bookProgress/${widget.bookId}/1/0');
                         }),
                         if (bookPage != 0 && isDone == false)
@@ -237,8 +237,12 @@ class _BookDetailState extends State<BookDetail> {
                   child: GreenButton("문제풀기", onPressed: () {
                     // Navigator.of(context).pop("refresh");
                     // context.go(RoutePath.main3);
-                    Navigator.of(context).pop();
-                    globalRouter.pushReplacement('/main/3/${widget.bookId}');
+                    if (isRead) {
+                      Navigator.of(context).pop();
+                      globalRouter.pushReplacement('/main/3/${widget.bookId}');
+                    } else {
+                      showToast('동화책을 읽고 진행해주세요', backgroundColor: AppColors.error);
+                    }
                     // globalRouter.pushReplacement(RoutePath.main3.replaceAll(":bookId", widget.bookId.toString()));
                     // Navigator.of(context).pop();
                     // context.pushReplacement(RoutePath.main3);
@@ -252,175 +256,5 @@ class _BookDetailState extends State<BookDetail> {
               ],
             ),
           );
-    return DefaultTextStyle(
-      style: CustomFontStyle.getTextStyle(context, CustomFontStyle.titleSmall),
-      child: Stack(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.06,
-              ),
-              Image.asset(
-                AppIcons.parchment,
-                height: MediaQuery.of(context).size.height * 0.88,
-              ),
-            ],
-          ),
-          Positioned(
-            left: MediaQuery.of(context).size.width * 0.3,
-            top: 0,
-            child: Container(
-              color: Colors.transparent,
-              child: Center(
-                child: Image.asset(AppIcons.bottle,
-                    width: MediaQuery.of(context).size.width * 0.35),
-              ),
-            ),
-          ),
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.06,
-            left: MediaQuery.of(context).size.width * 0.32,
-            child: Container(
-              height: MediaQuery.of(context).size.height * 0.1,
-              width: MediaQuery.of(context).size.width * 0.25,
-              color: Colors.transparent,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(
-                    bookTitle,
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.115,
-            left: MediaQuery.of(context).size.width * 0.045,
-            child: GestureDetector(
-              onTap: () {
-                Navigator.of(context).pop();
-              },
-              child: CircleBackIcon(
-                size: MediaQuery.of(context).size.width * 0.04,
-              ),
-            ),
-          ),
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.2,
-            left: MediaQuery.of(context).size.width * 0.13,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: CachedNetworkImage(
-                imageUrl: url,
-                fit: BoxFit.cover,
-                width: MediaQuery.of(context).size.width * 0.25,
-                placeholder: (context, url) =>
-                    const CircularProgressIndicator(),
-                errorWidget: (context, url, error) => const Icon(Icons.error),
-              ),
-            ),
-          ),
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.18,
-            right: MediaQuery.of(context).size.width * 0.1,
-            child: Container(
-                width: MediaQuery.of(context).size.width * 0.4,
-                height: MediaQuery.of(context).size.height * 0.7,
-                padding: EdgeInsets.zero,
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  // 한 줄에 2개의 항목을 표시
-                  crossAxisSpacing: MediaQuery.of(context).size.width * 0.01,
-                  // 가로 간격
-                  mainAxisSpacing: MediaQuery.of(context).size.height * 0.01,
-                  // 세로 간격
-                  physics: const NeverScrollableScrollPhysics(),
-                  children: List.generate(educations.length, (index) {
-                    var education = educations[index];
-                    return Transform.rotate(
-                      angle: index % 2 == 0 ? 350 * pi / 180 : 10 * pi / 180,
-                      child: Stack(
-                        children: [
-                          Center(
-                            child: CachedNetworkImage(
-                              imageUrl:
-                                  Constant.s3BaseUrl + education["imagePath"],
-                              fit: BoxFit.cover,
-                              height: MediaQuery.of(context).size.height * 0.3,
-                              errorWidget: (context, url, error) =>
-                                  const Icon(Icons.error),
-                            ),
-                          ),
-                          Positioned(
-                            bottom: MediaQuery.of(context).size.height * 0.055,
-                            left: 0,
-                            child: SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.185,
-                              child: Text(
-                                education["wordName"],
-                                style: CustomFontStyle.getTextStyle(
-                                    context, CustomFontStyle.textSmall),
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  }),
-                )),
-          ),
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.75,
-            left: MediaQuery.of(context).size.width * 0.12,
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width * 0.27,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  GreenButton("처음부터", onPressed: () {
-                    Navigator.of(context).pop();
-                    globalRouter
-                        .pushReplacement('/bookProgress/${widget.bookId}/1/0');
-                  }),
-                  if (bookPage != 0)
-                    GreenButton("이어하기", onPressed: () {
-                      Navigator.of(context).pop();
-                      globalRouter.pushReplacement(
-                          '/bookProgress/${widget.bookId}/$bookPage/0');
-                    }),
-                ],
-              ),
-            ),
-          ),
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.115,
-            right: MediaQuery.of(context).size.width * 0.06,
-            child: GreenButton("문제풀기", onPressed: () {
-              // Navigator.of(context).pop("refresh");
-              // context.go(RoutePath.main3);
-              if (isRead) {
-                Navigator.of(context).pop();
-                globalRouter.pushReplacement('/main/3/${widget.bookId}');
-              } else {
-                showToast('동화책을 읽고 진행해주세요', backgroundColor: AppColors.error);
-              }
-              // globalRouter.pushReplacement(RoutePath.main3.replaceAll(":bookId", widget.bookId.toString()));
-              // Navigator.of(context).pop();
-              // context.pushReplacement(RoutePath.main3);
-            }),
-          ),
-          const Positioned(
-            bottom: 0,
-            right: 0,
-            child: donggleTalk(situation: "BOOK"),
-          ),
-        ],
-      ),
-    );
   }
 }
